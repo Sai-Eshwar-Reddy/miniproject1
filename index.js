fdf1f889c516cc2b462b192d8468e19de3160765
Sai-Eshwar-Reddy <saieshwarreddy.work@gmail.com>
2026-07-16 19:24:09 +0530

    Added edit page and updated user information handling

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const { faker } = require('@faker-js/faker');
+const methodoverride = require('method-override');
 const mysql = require('mysql2');
 const express = require('express');
 const app = express();
+
+app.use(methodoverride("_method"));
+app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs")
 const path = require('path');
 app.set("views",path.join(__dirname,"/views"));
@@ -40,6 +44,55 @@ app.get('/users',(req,res)=>{
         {
             let data = result;
             res.render("userinfo.ejs",{ data });
+        }
+    });
+});
+app.get('/users/:id/edit',(req,res)=>{
+    let {id} = req.params;
+    let q = `select *from users where id = ?`;
+    connection.query(q,[id],(err,result)=>{
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            let user = result[0];
+            res.render("edit.ejs",{user});
+        }
+    });
+});
+app.patch("/user/:id",(req,res)=>{
+    let {id} = req.params;
+    let { password:formpassword,username:newUsername } = req.body;
+    let q = `select *from users where id = ?`;
+    connection.query(q,[id],(err,result)=>{
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            let user = result[0];
+            if(user.passwordd !== formpassword)
+            {
+                res.send("Wrong password");
+                console.log(formpassword);
+            }
+            else
+            {
+                let uq='update users set userid = ? where id = ?';
+                connection.query(uq,[newUsername,id],(err)=>{
+                    if(err)
+                    {
+                        console.log(err);
+                    }
+                    else
+                    {
+                        res.redirect("/users");
+                    }
+                })
+            }
         }
     });
 });
